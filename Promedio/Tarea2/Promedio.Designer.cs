67d71d56f328dbd483f6fef9c43e5086855e6c42
hernandez1098 <124010644+ldhs19@users.noreply.github.com>
2023-02-10 17:24:57 -0600

    tarea2

---
+++ Promedio/Tarea2/Promedio.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Tarea2
+{
+    partial class Promedio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Nota1TextBox = new System.Windows.Forms.TextBox();
+            this.Nota2TextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Nota3TextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Nota4TextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Calcularbutton = new System.Windows.Forms.Button();
+            this.PromTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nota del  Primer Parcial:";
+            // 
+            // Nota1TextBox
+            // 
+            this.Nota1TextBox.Location = new System.Drawing.Point(243, 52);
+            this.Nota1TextBox.Name = "Nota1TextBox";
+            this.Nota1TextBox.Size = new System.Drawing.Size(100, 20);
+            this.Nota1TextBox.TabIndex = 1;
+            // 
+            // Nota2TextBox
+            // 
+            this.Nota2TextBox.Location = new System.Drawing.Point(243, 93);
+            this.Nota2TextBox.Name = "Nota2TextBox";
+            this.Nota2TextBox.Size = new System.Drawing.Size(100, 20);
+            this.Nota2TextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nota del  Segundo Parcial:";
+            // 
+            // Nota3TextBox
+            // 
+            this.Nota3TextBox.Location = new System.Drawing.Point(243, 139);
+            this.Nota3TextBox.Name = "Nota3TextBox";
+            this.Nota3TextBox.Size = new System.Drawing.Size(100, 20);
+            this.Nota3TextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nota del  Tercer Parcial:";
+            // 
+            // Nota4TextBox
+            // 
+            this.Nota4TextBox.Location = new System.Drawing.Point(243, 183);
+            this.Nota4TextBox.Name = "Nota4TextBox";
+            this.Nota4TextBox.Size = new System.Drawing.Size(100, 20);
+            this.Nota4TextBox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(103, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Nota del Cuarto Parcial:";
+            // 
+            // Calcularbutton
+            // 
+            this.Calcularbutton.Location = new System.Drawing.Point(189, 276);
+            this.Calcularbutton.Name = "Calcularbutton";
+            this.Calcularbutton.Size = new System.Drawing.Size(75, 23);
+            this.Calcularbutton.TabIndex = 8;
+            this.Calcularbutton.Text = "Calcular";
+            this.Calcularbutton.UseVisualStyleBackColor = true;
+            this.Calcularbutton.Click += new System.EventHandler(this.Calcularbutton_Click);
+            // 
+            // PromTextBox
+            // 
+            this.PromTextBox.Location = new System.Drawing.Point(243, 217);
+            this.PromTextBox.Name = "PromTextBox";
+            this.PromTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PromTextBox.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(147, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Promedio:";
+            // 
+            // Promedio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PromTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Calcularbutton);
+            this.Controls.Add(this.Nota4TextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Nota3TextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Nota2TextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Nota1TextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Promedio";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Nota1TextBox;
+        private System.Windows.Forms.TextBox Nota2TextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Nota3TextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Nota4TextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Calcularbutton;
+        private System.Windows.Forms.TextBox PromTextBox;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Promedio/Tarea2/Promedio.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea2
+{
+    public partial class Promedio : Form
+    {
+        public Promedio()
+        {
+            InitializeComponent();
+        }
+
+        private void Calcularbutton_Click(object sender, EventArgs e)
+        {
+            int nota1 = 0;
+            int nota2 = 0;
+            int nota3 = 0;
+            int nota4 = 0;
+            int prom = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            nota1 = Convert.ToInt32(Nota1TextBox.Text);
+            nota2 = Convert.ToInt32(Nota2TextBox.Text);
+            nota3 = Convert.ToInt32(Nota3TextBox.Text);
+            nota4 = Convert.ToInt32(Nota4TextBox.Text);
+
+            if (prom < 65)
+            {
+                MessageBox.Show("Reprobado");
+            }
+            else 
+            {
+                MessageBox.Show("Aprobado");
+            }
+            PromTextBox.Text=prom.ToString();
+            
+
+            
+}
